4aa1c6bba6f3503e0255d9c2f335f6ae1aeadbf6
hawkeye-e <heroselis@gmail.com>
2025-09-26 03:00:01 +0100

    version update

--- COM3D2_WildParty/WildParty.cs
+++ COM3D2_WildParty/WildParty.cs
@@ -18,7 +18,7 @@ namespace COM3D2.WildParty.Plugin
     {
         public const string PluginName = "WildParty";
         public const string GUID = "COM3D2.WildParty.Plugin";
-        public const string Version = "0.9.0";
+        public const string Version = "1.0.0";
 
         internal static ManualLogSource Log;
 
